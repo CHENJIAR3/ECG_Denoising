81f6d62c061186b9c512f2f5c472203cfa5ccea0
CHENJIAR3 <87299283+CHENJIAR3@users.noreply.github.com>
2024-08-16 12:57:31 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -20,26 +20,29 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 assert len(gpus) > 0, "Not enough GPU hardware devices available"
 for i in range(len(gpus)):
     tf.config.experimental.set_memory_growth(gpus[i], True)
+def Read_ECG_sample(datapath,lead_idx=0):
 
+    ecg1_all = []
+    ecgpaths = glob.glob(datapath + "*.mat")
+    for ecgpath in ecgpaths:
+        # 读取数据
+        '''
+        下面读取的是cpsc2018的数据集，并选择I导联，输入数据应该为(N,1024,12)
+        '''
+        ecg = sio.loadmat(ecgpath)['ECG'][0][0][2]
+        ecg1 = ecg[lead_idx,:]
+        ecg1_all.append(ecg1)
+
+    return ecg1_all
 if __name__=='__main__':
     args.samplemethod = 'ddpm'
     modelpath = "./Denoiser"
-    fs = 100
+    fs = 500
+    datapath = './Sample_data/'
     Denoiser_1=Denoiser(modelpath)
-
-    model = tf.keras.models.load_model(modelpath)
-    forward_noiser=ForwardDiffusion(args.time_steps)
-    alphas = forward_noiser.alphas
-    betas = forward_noiser.betas
-    alpha_hats =forward_noiser.alpha_hat
-    ecg_dir = "original_ecg_path"
-    deecg_dir = "denoised_ecg_path"
-    if not os.path.exists(deecg_dir):
-        os.mkdir(deecg_dir)
-    pkl_files = glob.glob(gecg_dir+"*.pkl")
-    for path in pkl_files:
-        # pass
-        print(path)
-        with open(path, 'rb') as file:
-            data = pickle.load(file)
-        denoised_ecg=Denoising_ECG(Denoiser_1,data["gen_ecg"],fs)
+    try:
+        model = tf.keras.models.load_model(modelpath)
+        original_ecg = Read_ECG_sample(datapath=datapath)
+        denoised_ecg=Denoising_ECG(Denoiser_1,original_ecg,fs)
+    except:
+        pass
